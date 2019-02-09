32cdb57e5d8cb1a916276f05ffb7e1071db259ae
Vexatori <44672606+Vexatori@users.noreply.github.com>
2019-02-09 16:39:44 +0300

    TimerUpdate

--- MailSender/UtilityClasses/Timer.cs
+++ MailSender/UtilityClasses/Timer.cs
@@ -1,33 +1,27 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows.Threading;
+using GalaSoft.MvvmLight;
 
 namespace MailSender.UtilityClasses
 {
-    public class Timer : INotifyPropertyChanged
+    public class Timer : ViewModelBase
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
         private DateTime _now;
 
         public Timer()
         {
             _now = DateTime.Now;
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan( 0, 0, 1 );
-            timer.Tick += new EventHandler( timer_Tick );
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += new EventHandler(timer_Tick);
             timer.Start();
         }
 
         public DateTime Now
         {
             get => _now;
-            private set
-            {
-                _now = value;
-                if ( PropertyChanged != null )
-                    PropertyChanged( this, new PropertyChangedEventArgs( "Now" ) );
-            }
+            private set => Set(ref _now, value);
         }
 
         void timer_Tick( object sender, EventArgs e )
