2de6e0de44f3b06fc4345acec76073cb2f477cf6
Vexatori <44672606+Vexatori@users.noreply.github.com>
2019-02-09 11:44:25 +0300

    Recipients Interface

---
+++ MailSender.lib/Interfaces/IRecipientsData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSender.lib.Data.Linq2SQL;
+
+namespace MailSender.lib.Interfaces
+{
+    public interface IRecipientsData
+    {
+        IEnumerable<Recipient> GetAll();
+
+        Recipient GetById( int id );
+
+        void AddNew( Recipient newRecipient );
+
+        void Delete( int id );
+
+        void SaveChanges();
+    }
+}
