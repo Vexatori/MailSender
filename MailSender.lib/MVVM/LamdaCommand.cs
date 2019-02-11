f99c02d253854812e611f63acbea639c13da06be
Vexatori <44672606+Vexatori@users.noreply.github.com>
2019-02-11 10:31:34 +0300

    LamdaCommand

--- MailSender.lib/MVVM/LamdaCommand.cs
+++ MailSender.lib/MVVM/LamdaCommand.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MailSender.lib.MVVM
 {
-    class LamdaCommand
+    public class LamdaCommand : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private readonly Action<object> _onExecute;
+        private readonly Func<object, bool> _canExecute;
+
+        public LamdaCommand( Action<object> OnExecute, Func<object, bool> CanExecute = null )
+        {
+            _onExecute = OnExecute;
+            _canExecute = CanExecute;
+        }
+
+        public bool CanExecute( object parameter )
+        {
+            return _canExecute?.Invoke( parameter ) ?? true;
+        }
+
+        public void Execute( object parameter )
+        {
+            _onExecute( parameter );
+        }
     }
 }
--- MailSender/ViewModel/MainWindowViewModel.cs
+++ MailSender/ViewModel/MainWindowViewModel.cs
@@ -15,7 +15,7 @@ namespace MailSender.ViewModel
     {
         private IRecipientsData _recipientsData;
 
-        private ObservableCollection<Recipient> _recipients;
+        //private ObservableCollection<Recipient> _recipients;
 
         private string _Title = "Рассыльщик почты";
 
@@ -38,15 +38,17 @@ namespace MailSender.ViewModel
             get => new Timer().Now;
         }
 
-        public ObservableCollection<Recipient> Recipients
-        {
-            get
-            {
-                if ( _recipients != null ) return _recipients;
-                _recipients = new ObservableCollection<Recipient>(_recipientsData.GetAll());
-                return _recipients;
-            }
-        }
+        public ObservableCollection<Recipient> Recipients { get; } = new ObservableCollection<Recipient>();
+
+        //public ObservableCollection<Recipient> Recipients
+        //{
+        //    get
+        //    {
+        //        if ( _recipients != null ) return _recipients;
+        //        _recipients = new ObservableCollection<Recipient>(_recipientsData.GetAll());
+        //        return _recipients;
+        //    }
+        //}
 
         public MainWindowViewModel( IRecipientsData recipientsData )
         {
